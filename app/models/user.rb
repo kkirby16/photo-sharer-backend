8c0bbe63e42766bcdae3c54a2510fa0fb350abce
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-05 18:05:54 -0700

    add relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   #   we want our users to have passwords and we know we want to use bcrypt to hash them
+  has_many :comments
+  has_many :posts, through: :comments
+  has_many :posts
+  #shouldn't a user have many posts as well (has_many :posts) just because they can should be able to create posts themselves?
 end
