a6df0dc3cc32cc04faf28c34f713618a8b8edaf5
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-07 21:18:02 -0700

    add mock helpers for current_user and logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  helpers do
+    def current_user
+      User.first
+      #adding mocked version of being logged in.
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
