fe0f0096257dd6fb5071e05c0ec783517112be34
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-10 21:25:48 -0700

    add to create action so @user if exists is authenticated and logged in or error happens

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id #this logs the user in. putting the user's id in the session hash.
+      render json: @user
+    else
+      render json: {
+               error: "Invalid Credentials",
+             }
+    end
   end
 end
