901983803094e5e6014cb94350b1635a541c06ba
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-11 01:31:08 -0700

    syntax change & add include ::ActionController::Cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-  helpers do
-    def current_user
-      User.find(session[:user_id])
-      #adding mocked version of being logged in.
-    end
+  include ::ActionController::Cookies #also need to tell our controllers that we turned this stuff on.
 
-    def logged_in?
-      !!current_user
-    end
+  def current_user
+    User.find_by(id: session[:user_id])
+    #adding mocked version of being logged in.
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
