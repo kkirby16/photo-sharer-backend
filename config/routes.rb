e07e5690fcebf54433bb8b26e4283eabf41fa77b
kkirby16 <kkirby16@my.whitworth.edu>
2021-09-13 22:18:17 -0700

    add route for getting all posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create" #creating a session that will come from a post request.
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  get "/api/v1/get_all_posts", to: "api/v1/posts#index"
 
   namespace :api do
     namespace :v1 do
