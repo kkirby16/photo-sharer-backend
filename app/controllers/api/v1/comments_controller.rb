d18dd132c250414a1b2ced2c56e14afbaa3baaef
kkirby16 <kkirby16@my.whitworth.edu>
2021-10-01 14:48:56 -0700

    make create action for comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+
+    #check for login of the user.
+    #could use created at for the date.
+
+    @comment = Comment.create({ user_id: current_user.id, post_id: @post.id, text: params[:comment] })
+
+    @post.comments.push(@comment)
+    @post.save
+
+    # @post.comments.build({ user_id: @post.user, text: params[:comment] })
+    render json: @comment
+  end
 end
