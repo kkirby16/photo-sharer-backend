b4b8413b30f6e456bc701e29612747b5132c3f65
kkirby16 <kkirby16@my.whitworth.edu>
2021-10-11 22:18:44 -0700

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create(name: "Dan", username: "dansmith", password: "password")
 
 # like1 = Like.create(user_id: 3, post_id: 1)
 
-Post.create(user_id: 4, caption: "I will remember this drive forever!", seeded_image_data: "https://photos.smugmug.com/Valley-of-Fire/i-VNcfzCM/0/a6a2c2f6/L/0S6A0811-L.jpg")
+# Post.create(user_id: 4, caption: "I will remember this drive forever!", seeded_image_data: "https://photos.smugmug.com/Valley-of-Fire/i-VNcfzCM/0/a6a2c2f6/L/0S6A0811-L.jpg")
 Post.create(user_id: 3, caption: "Check this out!", seeded_image_data: "https://scontent-sea1-1.xx.fbcdn.net/v/t1.15752-9/243344596_1485820005110117_4987792217149552732_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=ae9488&_nc_ohc=du-uf1wFs3EAX8lfZHa&_nc_ht=scontent-sea1-1.xx&oh=b6f57b2b65cfc3c0b0d11869d0895b5c&oe=61807430")
 Post.create(user_id: 4, caption: "Look at this!", seeded_image_data: "https://scontent-sea1-1.xx.fbcdn.net/v/t1.15752-9/242919230_403323274697101_2355122384663906847_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=ae9488&_nc_ohc=pEaQszAqUGgAX9IQifQ&_nc_ht=scontent-sea1-1.xx&oh=641ff925868ba93bade17cd70fd13945&oe=617D2E28")
 Post.create(user_id: 2, caption: "Love this capture I got!", seeded_image_data: "https://scontent-sea1-1.xx.fbcdn.net/v/t1.15752-9/242817805_169981778621698_8980700211675454975_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=ae9488&_nc_ohc=vm0pszRzWCEAX-stAEs&tn=YfmM6eeA2bmdZyBS&_nc_ht=scontent-sea1-1.xx&oh=eae9c53edf2980cf4cd40f9dc28a2002&oe=617F6BBD")
