8a4874d663fd419a8caa08192481a96a74326c9b
kkirby16 <kkirby16@my.whitworth.edu>
2021-10-25 17:52:44 -0700

    add a comment for myself

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -4,7 +4,7 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :user, :caption, :likes, :image, :image_url, :seeded_image_data
   has_many :comments, serializer: CommentSerializer
 
-  def image
+  def image #within the image key of each post there will be an object with a url key set to the url for the image"
     if object.image.attached?
       {
         url: "http://localhost:4500#{rails_blob_path(object.image, only_path: true)}",
