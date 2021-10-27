635cdba03436540be58a2029c59b355b9b1ee3bf
kkirby16 <kkirby16@my.whitworth.edu>
2021-10-26 21:54:41 -0700

    change to use heroku url in customer image method

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -7,7 +7,7 @@ class PostSerializer < ActiveModel::Serializer
   def image #within the image key of each post there will be an object with a url key set to the url for the image"
     if object.image.attached?
       {
-        url: "http://localhost:4500#{rails_blob_path(object.image, only_path: true)}",
+        url: "https://photo-sharer-backend.herokuapp.com/#{rails_blob_path(object.image, only_path: true)}",
       }
     end
   end
