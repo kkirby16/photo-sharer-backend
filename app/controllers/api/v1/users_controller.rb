ccdb33f1f2e994195ad3a452cb06a0ba17eeb599
kkirby16 <kkirby16@my.whitworth.edu>
2021-12-14 22:11:02 -0800

    change before_action back to how it was

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate_user, only: [:show, :index, :update, :destroy]
+  before_action :authenticate_user, except: :create
   before_action :set_user, only: [:show, :update, :destroy]
 
   #  GET /users
