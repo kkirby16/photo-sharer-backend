880e472135878d5b90a54c5077e4f3a52b11701e
kkirby16 <kkirby16@my.whitworth.edu>
2022-02-04 00:00:05 -0800

    take out comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :likes
-  # validates :image, :caption, presence: true
 
   def image_url
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
