716f8e2ee51f84ceb8fafc5bb8ca9826ab42130c
kkirby16 <kkirby16@my.whitworth.edu>
2023-09-18 15:35:41 -0700

    add another line of code to production.rb to try to get backend image assets to be available in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,4 +107,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.serve_static_assets = true
+  config.assets.digest = true
 end
